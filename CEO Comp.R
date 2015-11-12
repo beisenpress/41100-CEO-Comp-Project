2026ae32351901ff95e261c539512011cc96c9e8
beisenpress <ben@eisenpress.com>
2015-11-12 11:14:05 -0600

    Initial data import and merge

--- CEO Comp.R
+++ CEO Comp.R
@@ -1,3 +1,10 @@
 setwd("/Users/ben/dropbox/Chicago Booth/41100 Regressions/Project")
 
-ExecuComp <- read.csv("Execucomp 1.csv")
+# Import compensation data from Compustat ExecuComp dataset
+ExecuComp <- read.csv("Execucomp GVKey All.csv")
+
+# Import financials data from Compustat North America dataset
+Financials <- read.csv("NA GVKey 1.csv")
+
+# Merge two dataset
+combined <- merge(ExecuComp,Financials, by = "GVKEY")
