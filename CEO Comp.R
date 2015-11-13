636a05bbda067ae2eae556c962514da98c551d69
beisenpress <ben@eisenpress.com>
2015-11-13 11:31:24 -0600

    Added Log to regression

--- CEO Comp.R
+++ CEO Comp.R
@@ -26,10 +26,16 @@ CEO_Errors <- combined[which(combined$CEO_Flag0 != combined$CEO_Flag3),]
 # Create a flag for CEO using either the PCEO variable or the CEOANN variable or the TITLE Variable
 combined$CEO_Flag0 <- combined$CEO_Flag1|combined$CEO_Flag2|combined$CEO_Flag3
 
+# Get dataset of all CEOs
 ceo <- combined[which(combined$CEO_Flag0 == 1),]
 
 ######### Simple Regression  #######
 
+# Drop observations where CEO has positive pay
+ceo1 <- ceo[which(ceo$TDC1 >0),]
+
 # Regress total compensation on total assets, total Cash, Dividents, and total liabilities
-reg1 <- lm(TDC1 ~ at + ch + dvt +  lt, data = ceo)
+reg1 <- lm(log(TDC1) ~ at + ch + dvt +  lt, data = ceo1)
 summary(reg1)
+
+plot(reg1$fitted.values,rstudent(reg1), pch=20, main = "Fitted Values and Studentized Residuals")
